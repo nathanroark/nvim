58c51b1e5cdc9790c083a575dd38bae7385e64ad
Nathan Roark <nathanroark@protonmail.com>
2023-07-29 15:30:59 -0500

    added os clipboard keymap

--- init.lua
+++ init.lua
@@ -233,6 +233,14 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
